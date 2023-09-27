d565c020640ccf2f4034b7e1f3711e5bafb9d9d1
hyun1024 <haema3043@gmail.com>
2023-09-27 22:51:29 +0900

    Update : change discount policy From Fix to Rate

--- src/main/java/com/inf/infp1/AppConfig.java
+++ src/main/java/com/inf/infp1/AppConfig.java
@@ -1,6 +1,8 @@
 package com.inf.infp1;
 
-import com.inf.infp1.discount.FixDiscountPolicy;
+import com.inf.infp1.discount.DiscountPolicy;
+import com.inf.infp1.discount.RateDiscountPolicy;
+import com.inf.infp1.member.MemberRepository;
 import com.inf.infp1.member.MemberService;
 import com.inf.infp1.member.MemberServiceImpl;
 import com.inf.infp1.member.MemoryMemberRepository;
@@ -9,10 +11,18 @@ import com.inf.infp1.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new RateDiscountPolicy();
     }
 }
