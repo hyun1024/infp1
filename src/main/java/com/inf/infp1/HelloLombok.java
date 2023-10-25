a72113eceefa0b5e55b0bf3c013119fe25ef89bb
hyun1024 <haema3043@gmail.com>
2023-10-25 20:31:18 +0900

    Feat : Lombok getter and setter

---
+++ src/main/java/com/inf/infp1/HelloLombok.java
@@ -0,0 +1,19 @@
+package com.inf.infp1;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloLombok {
+
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        HelloLombok helloLombok = new HelloLombok();
+        helloLombok.setName("asdfas");
+
+        System.out.println("helloLombok = " + helloLombok);
+    }
+}
