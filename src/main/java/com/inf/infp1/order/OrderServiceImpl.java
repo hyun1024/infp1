f7a5eee04e88f483f49c8b3982840564439dd332
hyun1024 <haema3043@gmail.com>
2023-10-25 20:33:24 +0900

    Feat : set RequiredArgsConstructor

--- src/main/java/com/inf/infp1/order/OrderServiceImpl.java
+++ src/main/java/com/inf/infp1/order/OrderServiceImpl.java
@@ -3,18 +3,14 @@ package com.inf.infp1.order;
 import com.inf.infp1.discount.DiscountPolicy;
 import com.inf.infp1.member.Member;
 import com.inf.infp1.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
