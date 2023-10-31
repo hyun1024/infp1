7fea943220a5f28fee5d9a45fe411fc254769206
hyun1024 <haema3043@gmail.com>
2023-10-31 20:37:34 +0900

    Feat : bean callback annotation

--- src/test/java/com/inf/infp1/lifecycle/NetworkClient.java
+++ src/test/java/com/inf/infp1/lifecycle/NetworkClient.java
@@ -1,35 +1,40 @@
 package com.inf.infp1.lifecycle;
 
-public class NetworkClient{
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
+public class NetworkClient {
 
     private String url;
-    public NetworkClient(){
+
+    public NetworkClient() {
         System.out.println("생성자 호출: url = " + url);
     }
 
-    public void setUrl(String url){
+    public void setUrl(String url) {
         this.url = url;
     }
 
-    public void connect(){
-        System.out.println("connect : "+url);
-    }
-    public void call(String message){
-        System.out.println("call : "+url + " message = " + message);
+    public void connect() {
+        System.out.println("connect : " + url);
     }
 
-    public void disconnect(){
-        System.out.println("close : "+url);
+    public void call(String message) {
+        System.out.println("call : " + url + " message = " + message);
     }
 
+    public void disconnect() {
+        System.out.println("close : " + url);
+    }
 
-    public void init() throws Exception{
+    @PostConstruct
+    public void init() throws Exception {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-
+    @PreDestroy
     public void close() throws Exception {
         System.out.println("NetworkClient.close start");
         disconnect();
