7afa531bd0ab3e4231e620adddac5cd272454c7a
hyun1024 <haema3043@gmail.com>
2023-11-12 22:33:30 +0900

    Feat : set ObjectProvider

--- src/main/java/com/inf/infp1/web/LogDemoController.java
+++ src/main/java/com/inf/infp1/web/LogDemoController.java
@@ -3,22 +3,23 @@ package com.inf.infp1.web;
 import com.inf.infp1.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@Configuration
+@Controller
 @RequiredArgsConstructor
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
-    @RequestMapping("log-demo")
+    @RequestMapping("/log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request){
        String requestURL = request.getRequestURL().toString();
+       MyLogger myLogger = myLoggerProvider.getObject();
        myLogger.setRequestURL(requestURL);
 
        myLogger.log("controller test");
